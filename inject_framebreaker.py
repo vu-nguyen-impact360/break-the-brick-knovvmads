608650d5839e6a8ff63a933485317eaa14dd1571
Ben Chong <ben@hotbazooka.com>
2013-09-25 21:33:01 +0800

    framebreaker

--- inject_framebreaker.py
+++ inject_framebreaker.py
@@ -2,7 +2,7 @@ file1 = open('domainlock.js')
 file1_contents = file1.read()
 
 # Inject framebreaker code
-restricted_domain = 'marketjs' # should be https://marketjs.com, once SSL is up
+restricted_domain = 'marketjs.com' # should be https://marketjs.com, once SSL is up
 
 framebreaker = 'if(document.referrer.indexOf(\"' + restricted_domain + '\")<0){if(top!=self){console.log(\"showing anti-piracy layer ...\");$(\"#anti-piracy\").show();top.location.replace(self.location.href);}}'
 # console.log(\"document.referrer\",document.referrer);console.log(\"framebreaker test ...\");
