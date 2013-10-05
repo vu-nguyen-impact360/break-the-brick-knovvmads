fada3edff19c5f213dfdb07e6a58984e483f6f58
Ben Chong <ben@hotbazooka.com>
2013-10-05 16:54:17 +0800

    somefix

--- glue/soundjs/sound-manager.js
+++ glue/soundjs/sound-manager.js
@@ -1,4 +1,5 @@
 var manifest = [
+	{src:"media/audio/play/static.ogg|media/audio/play/static.mp3", id:"static"},
 	{src:"media/audio/opening/opening.ogg|media/audio/opening/opening.mp3", id:"opening"},
     {src:"media/audio/start.ogg|media/audio/start.mp3", id:"start"},
 ];
