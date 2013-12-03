89207a3088385972c34b21ebb23fd1e5a2ea2793
Ben Chong <ben@marketjs.com>
2013-12-03 14:05:43 +0800

    htc one fix

--- glue/soundjs/sound-manager.js
+++ glue/soundjs/sound-manager.js
@@ -1,3 +1,4 @@
+try{
 var manifest = [
 	{src:"media/audio/play/static.ogg|media/audio/play/static.mp3", id:"static"},
 	{src:"media/audio/opening/opening.ogg|media/audio/opening/opening.mp3", id:"opening"},
@@ -6,3 +7,4 @@ var manifest = [
 
 createjs.Sound.registerPlugins([createjs.HTMLAudioPlugin, createjs.WebAudioPlugin]);
 createjs.Sound.registerManifest(manifest);
+}catch(e){console.log('soundJS: '+e);}
