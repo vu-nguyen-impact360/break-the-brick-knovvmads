a784debbdafb97b2afcc4d3d5fd3caeabfb5088a
fedyyapary <fedy.yapary@impact360design.com>
2018-07-11 09:05:54 +0800

    JScrambler Update, Audio Context & Fixed Some Bugs

--- media/text/strings.js
+++ media/text/strings.js
@@ -1,68 +1,57 @@
 var _STRINGS = {
-	"Ad":{
-		"Mobile":{
-			"Preroll":{
-				"ReadyIn":"The game is ready in ",
-				"Loading":"Your game is loading...",
-				"Close":"Close",
+	"Ad": {
+		"Mobile": {
+			"Preroll": {
+				"ReadyIn": "The game is ready in ",
+				"Loading": "Your game is loading...",
+				"Close": "Close",
 			},
-			"Header":{
-				"ReadyIn":"The game is ready in ",
-				"Loading":"Your game is loading...",
-				"Close":"Close",
+			"Header": {
+				"ReadyIn": "The game is ready in ",
+				"Loading": "Your game is loading...",
+				"Close": "Close",
 			},
-			"End":{
-				"ReadyIn":"Advertisement ends in ",
-				"Loading":"Please wait ...",
-				"Close":"Close",
-			},								
+			"End": {
+				"ReadyIn": "Advertisement ends in ",
+				"Loading": "Please wait ...",
+				"Close": "Close",
+			},
 		},
 	},
-	
-	"Splash":{
-		"Loading":"Loading ...",	
-		"LogoLine1":"Some text here",
-		"LogoLine2":"powered by MarketJS",
-		"LogoLine3":"none",						
+	"Splash": {
+		"Loading": "Loading ...",
+		"LogoLine1": "Some text here",
+		"LogoLine2": "powered by MarketJS",
+		"LogoLine3": "none",
+		"TapToStart": "TAP TO START"
 	},
-
-	"Game":{
-		"SelectPlayer":"Select Player",
-		"Win":"You win!",
-		"Lose":"You lose!",
-		"Score":"Score",
-		"Time":"Time",
-		"Next":"next",
-		
-		"toturial01_01":"Learn how to play?",
-		
-		"toturial02_01":"Click and drag the paddle",
-		"toturial02_02":"to move it horizontally.",
-		"toturial02_03":"Drag the paddle to control it horizontally.",
-		
-		"toturial03_01":"The ball will bounce off the paddle.",
-		"toturial03_02":"Control the direction",
-		"toturial03_03":"of each bounce wisely.",
-		
-		"toturial04_01":"To break the brick, aim your ball against it.",
-		"toturial04_02":"Normal bricks need to be hit twice.",
-		"toturial04_03":"Cracked bricks will break after a single hit.",
-		
-		"toturial05_01":"Iron blocks can't be broken,",
-		"toturial05_02":"plus they increase the speed of the ball.",
-		"toturial05_03":"Try to avoid them if you can.",
-		
-		"toturial06_01":"Destroying bricks will occassionally",
-		"toturial06_02":"produce power-ups.",
-		"toturial06_03":"Catch them with your paddle!",
-		
-		"toturial07_01":"Are you ready?",
-		"toturial07_02":"Let's break some bricks!",
-		
-		
+	"Game": {
+		"SelectPlayer": "Select Player",
+		"Win": "You win!",
+		"Lose": "You lose!",
+		"Score": "Score",
+		"Time": "Time",
+		"Next": "next",
+		"toturial01_01": "Learn how to play?",
+		"toturial02_01": "Click and drag the paddle",
+		"toturial02_02": "to move it horizontally.",
+		"toturial02_03": "Drag the paddle to control it horizontally.",
+		"toturial03_01": "The ball will bounce off the paddle.",
+		"toturial03_02": "Control the direction",
+		"toturial03_03": "of each bounce wisely.",
+		"toturial04_01": "To break the brick, aim your ball against it.",
+		"toturial04_02": "Normal bricks need to be hit twice.",
+		"toturial04_03": "Cracked bricks will break after a single hit.",
+		"toturial05_01": "Iron blocks can't be broken,",
+		"toturial05_02": "plus they increase the speed of the ball.",
+		"toturial05_03": "Try to avoid them if you can.",
+		"toturial06_01": "Destroying bricks will occassionally",
+		"toturial06_02": "produce power-ups.",
+		"toturial06_03": "Catch them with your paddle!",
+		"toturial07_01": "Are you ready?",
+		"toturial07_02": "Let's break some bricks!",
 	},
-
-	"Results":{
-		"Title":"High score",
-	},	
+	"Results": {
+		"Title": "High score",
+	},
 };
