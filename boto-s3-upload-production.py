bffb209a7da5df997c99152af36e465c0d4f3d3d
Yew Meng <yew.meng@marketjs.com>
2022-09-29 12:38:32 +0800

    Update boto scripts

--- boto-s3-upload-production.py
+++ boto-s3-upload-production.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
  MarketJS Amazon S3 Deployment System
  -----------------------------------------------------------------------
@@ -14,7 +15,7 @@ from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
 """ OWN SERVER """
-conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID'],os.environ['AWS_SECRET_ACCESS_KEY'])
+conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID_PRODUCTION'],os.environ['AWS_SECRET_ACCESS_KEY_PRODUCTION'])
 BUCKET_NAME = 'marketjs-factory'
 GAME_NAME = os.path.split(os.getcwd())[-1] # same as folder name
 LANGUAGE_CODE = None
@@ -23,8 +24,8 @@ BUCKET = conn.get_bucket(BUCKET_NAME)
 BUCKET_LOCATION = BUCKET.get_location()
 
 def usage():
-	print 'Options and arguments:'
-	print '-a --all	:  [uploads everything in folder]'
+	print ('Options and arguments:')
+	print ('-a --all	:  [uploads everything in folder]')
 	
 def uploadResultToS3(bucket,game_folder_name,srcDir):
 	
@@ -33,7 +34,7 @@ def uploadResultToS3(bucket,game_folder_name,srcDir):
 		opts, args = getopt.getopt(sys.argv[1:], "hanl:v", ["help","all","new","language"])
 	except getopt.GetoptError as err:
 		# print help information and exit:
-		print str(err) # will print something like "option -a not recognized"
+		print (str(err)) # will print something like "option -a not recognized"
 		usage()
 		sys.exit(2)
 	
@@ -51,12 +52,12 @@ def uploadResultToS3(bucket,game_folder_name,srcDir):
 			sys.exit()
 		elif o in ("-n", "--new"):
 			upload_all = False
-			print "upload all set True"	
+			print ("upload all set True")
 		elif o in ("-a", "--all"):
 			upload_all = True
-			print "upload all set True"				
+			print ("upload all set True")		
 		elif o in ("-l", "--language"):
-			print "language chosen:" + a
+			print ("language chosen:" + a)
 			LANGUAGE_CODE = a
 		else:
 			assert False, "unhandled option"
@@ -66,27 +67,27 @@ def uploadResultToS3(bucket,game_folder_name,srcDir):
 	k = Key(b)
 	
 	""" PATTERN MATCHING """	
-	file_pattern = re.compile(r'.*\.(md$|aif$|tiff$|au$|psd$|xcf$|sh$|py$|pyc$|php$|bat$|git$|gitignore$|txt$|jar$|DS_Store$)')
-	folder_pattern = re.compile(r'.*(/glue/|/glue|/lib/|/lib|/tools/|/tools|/git/|/git|git)')
-	folder_pattern_windows = re.compile(r'.*(\\glue\\|\\glue|\\lib\\|\\lib|\\tools\\|\\tools|git)')
+	file_pattern = re.compile(r'.*\.(md$|aif$|tiff$|au$|psd$|xcf$|sh$|py$|pyc$|php$|bat$|git$|gitignore$|gitkeep$|tm_properties$|txt$|jar$|DS_Store$)')
+	folder_pattern = re.compile(r'(\.git|.vscode|node_modules|glue|doc|config|lib|tools[\/\\]*?$)')
 
 	""" UPLOAD SETTINGS """
 	day_freshness = 1
 	seconds_freshness = 86400/2
 	
 	if upload_all:
-		print 'uploading ALL files in folders ...'
+		print ('uploading ALL files in folders ...')
 	else:		
-		print 'uploading files < ' + str(day_freshness) + ' days' + ' and < ' + str(seconds_freshness/3600) + ' hours old ...'
+		print ('uploading files < ' + str(day_freshness) + ' days' + ' and < ' + str(seconds_freshness/3600) + ' hours old ...')
 	
 	
 	""" WALKING THE BUCKET """
-	print 'preparing to walk the bucket named ' + b.name + '...'
+	print ('preparing to walk the bucket named ' + b.name + '...')
 			
 	for path,dir,files in os.walk(srcDir):
 		for file in files:			
 			""" filter out unwanted file extensions (eg: xcf,sh,py)"""
-			if not re.match(file_pattern,file) and not re.match(folder_pattern,path) and not re.match(folder_pattern_windows,path):
+			relativePath = os.path.relpath(path, srcDir)
+			if not re.match(file_pattern,file) and not re.match(folder_pattern,relativePath):
 
 				""" get freshness """
 				last_modified_time_epoch_seconds = os.path.getmtime(os.path.join(path,file))
@@ -94,24 +95,28 @@ def uploadResultToS3(bucket,game_folder_name,srcDir):
 				delta = datetime.now()-last_modified_time
 				
 				if upload_all:
-					upload(k,b,game_folder_name,path,file,srcDir,LANGUAGE_CODE)					
+					upload(k,b,game_folder_name,path,file.decode('utf8'),srcDir,LANGUAGE_CODE)					
 				else:					
 					if delta.days < day_freshness and delta.seconds < seconds_freshness:
-						upload(k,b,game_folder_name,path,file,srcDir,LANGUAGE_CODE)
+						upload(k,b,game_folder_name,path,file.decode('utf8'),srcDir,LANGUAGE_CODE)
 						
 def upload(k,b,game_folder_name,path,file,srcDir,language_code):
-	print 'Preparing bucket for upload'
+	print ('Preparing bucket for upload')
 	k.key = language_code + '/' + game_folder_name + "/" + os.path.relpath(os.path.join(path,file),srcDir)
 	k.key = re.sub(r'\\', '/', k.key) #added to avoid forward slash in k.key 
-	print 'sending ' + file + ' to https://s3-' + BUCKET_LOCATION + '.amazonaws.com/'  + b.name + '/' + k.key + ' ...'
+	print ('sending ' + file + ' to https://s3-' + BUCKET_LOCATION + '.amazonaws.com/'  + b.name + '/' + k.key + ' ...')
 	
-	k.set_contents_from_filename(os.path.join(path,file))
+	headers = {
+		'Cache-Control': 'max-age=7776000'
+	}
+	
+	k.set_contents_from_filename(os.path.join(path,file), headers)
 
 	if path.find('_factory') >=0:
-		print 'file set as private ...'
+		print ('file set as private ...')
 		k.set_acl('private')				
 	else:
-		print 'file set as public ...'
+		print ('file set as public ...')
 		k.set_acl('public-read')						
 
 		
--- boto-s3-upload.py
+++ boto-s3-upload.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
  MarketJS Amazon S3 Deployment System
  -----------------------------------------------------------------------
@@ -14,10 +15,10 @@ from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
 """ OWN SERVER """
-#conn = S3Connection('AKIAJT4I3IAFP7WEPBMQ', 'NhrbnwHsMUKMnfuKYqgEakf6PTOaoN+oWrhjU9UX')
-conn = S3Connection('AKIAJ2PO6DBGS5D7LPLQ', 'rVwJOlK+E9TnKgJeS4rC2zgvoWeNK/JLtcPV0xQP')
-#BUCKET_NAME = 'marketjs-lab'
-BUCKET_NAME = 'marketjs-lab2'
+conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID'],os.environ['AWS_SECRET_ACCESS_KEY'],host="s3.ap-southeast-1.amazonaws.com")
+# S3 Endpoints: s3.REGION.amazonaws.com https://docs.aws.amazon.com/general/latest/gr/s3.html
+# To access, goto http://s3.ap-southeast-1.amazonaws.com/marketjs-lab3/en/game_folder/index.html
+BUCKET_NAME = 'marketjs-lab3'
 GAME_NAME = os.path.split(os.getcwd())[-1] # same as folder name
 LANGUAGE_CODE = None
 
@@ -25,8 +26,8 @@ BUCKET = conn.get_bucket(BUCKET_NAME)
 BUCKET_LOCATION = BUCKET.get_location()
 
 def usage():
-	print 'Options and arguments:'
-	print '-a --all	:  [uploads everything in folder]'
+	print ('Options and arguments:')
+	print ('-a --all	:  [uploads everything in folder]')
 	
 def uploadResultToS3(bucket,game_folder_name,srcDir):
 	
@@ -35,7 +36,7 @@ def uploadResultToS3(bucket,game_folder_name,srcDir):
 		opts, args = getopt.getopt(sys.argv[1:], "hanl:v", ["help","all","new","language"])
 	except getopt.GetoptError as err:
 		# print help information and exit:
-		print str(err) # will print something like "option -a not recognized"
+		print (str(err)) # will print something like "option -a not recognized"
 		usage()
 		sys.exit(2)
 	
@@ -53,12 +54,12 @@ def uploadResultToS3(bucket,game_folder_name,srcDir):
 			sys.exit()
 		elif o in ("-n", "--new"):
 			upload_all = False
-			print "upload all set True"	
+			print ("upload all set True")
 		elif o in ("-a", "--all"):
 			upload_all = True
-			print "upload all set True"				
+			print ("upload all set True")		
 		elif o in ("-l", "--language"):
-			print "language chosen:" + a
+			print ("language chosen:" + a)
 			LANGUAGE_CODE = a
 		else:
 			assert False, "unhandled option"
@@ -68,55 +69,57 @@ def uploadResultToS3(bucket,game_folder_name,srcDir):
 	k = Key(b)
 	
 	""" PATTERN MATCHING """	
-	file_pattern = re.compile(r'.*\.(md$|aif$|tiff$|au$|psd$|xcf$|sh$|py$|pyc$|php$|bat$|git$|gitignore$|txt$|jar$|DS_Store$)')
-	folder_pattern = re.compile(r'.*(/glue/|/glue|/lib/|/lib|/tools/|/tools|/git/|/git|git)')
-	folder_pattern_windows = re.compile(r'.*(\\glue\\|\\glue|\\lib\\|\\lib|\\tools\\|\\tools|git)')
+	file_pattern = re.compile(r'.*\.(md$|aif$|tiff$|au$|psd$|xcf$|sh$|py$|pyc$|php$|bat$|git$|gitignore$|gitkeep$|tm_properties$|txt$|jar$|DS_Store$)')
+	folder_pattern = re.compile(r'(\.git|.vscode|node_modules|glue|doc|config|lib|tools[\/\\]*?$)')
 
 	""" UPLOAD SETTINGS """
 	day_freshness = 1
 	seconds_freshness = 86400/2
 	
 	if upload_all:
-		print 'uploading ALL files in folders ...'
+		print ('uploading ALL files in folders ...')
 	else:		
-		print 'uploading files < ' + str(day_freshness) + ' days' + ' and < ' + str(seconds_freshness/3600) + ' hours old ...'
+		print ('uploading files < ' + str(day_freshness) + ' days' + ' and < ' + str(seconds_freshness/3600) + ' hours old ...')
 	
 	
 	""" WALKING THE BUCKET """
-	print 'preparing to walk the bucket named ' + b.name + '...'
+	print ('preparing to walk the bucket named ' + b.name + '...')
 			
 	for path,dir,files in os.walk(srcDir):
 		for file in files:			
 			""" filter out unwanted file extensions (eg: xcf,sh,py)"""
-			if not re.match(file_pattern,file) and not re.match(folder_pattern,path) and not re.match(folder_pattern_windows,path):
-
+			relativePath = os.path.relpath(path, srcDir)
+			if not re.match(file_pattern,file) and not re.match(folder_pattern,relativePath):
 				""" get freshness """
 				last_modified_time_epoch_seconds = os.path.getmtime(os.path.join(path,file))
 				last_modified_time = datetime.fromtimestamp(last_modified_time_epoch_seconds)
 				delta = datetime.now()-last_modified_time
 				
 				if upload_all:
-					upload(k,b,game_folder_name,path,file,srcDir,LANGUAGE_CODE)					
+					upload(k,b,game_folder_name,path,file.decode('utf8'),srcDir,LANGUAGE_CODE)					
 				else:					
 					if delta.days < day_freshness and delta.seconds < seconds_freshness:
-						upload(k,b,game_folder_name,path,file,srcDir,LANGUAGE_CODE)
+						upload(k,b,game_folder_name,path,file.decode('utf8'),srcDir,LANGUAGE_CODE)
 						
 def upload(k,b,game_folder_name,path,file,srcDir,language_code):		
-	print 'Preparing bucket for upload'	
+	print ('Preparing bucket for upload')
 	k.key = language_code + '/' + game_folder_name + "/" + os.path.relpath(os.path.join(path,file),srcDir)
 	k.key = re.sub(r'\\', '/', k.key) #added to avoid forward slash in k.key
-	print 'sending ' + file + ' to https://s3-' + BUCKET_LOCATION + '.amazonaws.com/'  + b.name + '/' + k.key + ' ...'
+	print ('sending ' + file + ' to https://s3-' + BUCKET_LOCATION + '.amazonaws.com/'  + b.name + '/' + k.key + ' ...')
 	
-	k.set_contents_from_filename(os.path.join(path,file))
+	headers = {
+		'Cache-Control': 'max-age=7776000'
+	}
+	
+	k.set_contents_from_filename(os.path.join(path,file), headers)
 
 	if path.find('_factory') >=0:
-		print 'file set as private ...'
+		print ('file set as private ...')
 		k.set_acl('private')				
 	else:
-		print 'file set as public ...'
+		print ('file set as public ...')
 		k.set_acl('public-read')						
 
 		
 """ CHECK BEFORE RUNNING """
-uploadResultToS3(BUCKET_NAME,GAME_NAME, os.getcwd())
 uploadResultToS3(BUCKET_NAME,GAME_NAME, os.getcwd())
