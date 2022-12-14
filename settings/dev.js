ee497fdf579084786751d905e94d3c1b12d15c42
nguyen-chung-impact360 <chung.nguyen@impact360impact.com>
2022-12-14 13:39:32 +0700

    fixing gun in firing mod

--- settings/dev.js
+++ settings/dev.js
@@ -129,7 +129,7 @@ var _SETTINGS = {
     },
     'Versioning': {
         'Version': '1.0.0',
-        'Build': '97',
+        'Build': '99',
 
         'DisplayLog': false,
         'DrawVersion': false,
