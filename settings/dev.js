973696f0ab4fa3fe63d06eec0db7ce8dcb45edcc
nguyen-chung-impact360 <chung.nguyen@impact360impact.com>
2022-12-19 13:56:47 +0700

    update block sprite

--- settings/dev.js
+++ settings/dev.js
@@ -129,7 +129,7 @@ var _SETTINGS = {
     },
     'Versioning': {
         'Version': '1.0.0',
-        'Build': '101',
+        'Build': '102',
 
         'DisplayLog': false,
         'DrawVersion': false,
