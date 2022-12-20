67e747a5dc65972eec2a18e28ee99e2374390bfa
nguyen-chung-impact360 <chung.nguyen@impact360impact.com>
2022-12-20 16:19:09 +0700

    change block sprite sheet to individual sprite

--- settings/dev.js
+++ settings/dev.js
@@ -129,7 +129,7 @@ var _SETTINGS = {
     },
     'Versioning': {
         'Version': '1.0.0',
-        'Build': '104',
+        'Build': '105',
 
         'DisplayLog': false,
         'DrawVersion': false,
