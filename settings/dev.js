ca21549045a27377ca8d181ab49bf9d863ad4e26
nguyen-chung-impact360 <chung.nguyen@impact360impact.com>
2022-12-20 17:25:47 +0700

    remove the +100 margin since it causes the aligntment issue

--- settings/dev.js
+++ settings/dev.js
@@ -129,7 +129,7 @@ var _SETTINGS = {
     },
     'Versioning': {
         'Version': '1.0.0',
-        'Build': '105',
+        'Build': '106',
 
         'DisplayLog': false,
         'DrawVersion': false,
