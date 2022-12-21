2aed69625c7595a6eca026be296eda8a6ef9aa29
nguyen-chung-impact360 <chung.nguyen@impact360impact.com>
2022-12-21 13:30:55 +0700

    replace result text boxes

--- settings/dev.js
+++ settings/dev.js
@@ -129,7 +129,7 @@ var _SETTINGS = {
     },
     'Versioning': {
         'Version': '1.0.0',
-        'Build': '107',
+        'Build': '108',
 
         'DisplayLog': false,
         'DrawVersion': false,
